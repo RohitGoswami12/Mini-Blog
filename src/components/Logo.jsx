355840ea78ae5bb464057aa807327b81d7b8231e
Rohit Goswami <rohitkumargoswami31@gmail.com>
2025-01-11 20:22:00 +0530

    Adding Logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-function Logo({width = '100px'}) {
+function Logo({ width = '100px' }) {
   return (
-    <div>Logo</div>
-  )
+    <div>
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/60/60736.png"
+        width={'40px'}
+      />
+    </div>
+  );
 }
 
-export default Logo
+export default Logo;
